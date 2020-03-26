3ba8a2b3beaa5c888b61ffd2f7d3c9804003ba08
Ahmad Dani <a.dani.18425021@student.ubl.ac.id>
2020-03-26 18:49:45 +0700

    first commit

---
+++ latihan1/src/router/routes.js
@@ -0,0 +1,26 @@
+const routes = [
+  {
+    path: '/',
+    component: () => import('layouts/loginlayouts.vue'),
+    children: [
+      { path: '', component: () => import('pages/login.vue') }
+    ]
+  },
+  {
+    path: '/home',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [
+      { path: '', component: () => import('pages/login.vue') }
+    ]
+  }
+]
+
+// Always leave this as last one
+if (process.env.MODE !== 'ssr') {
+  routes.push({
+    path: '*',
+    component: () => import('pages/Error404.vue')
+  })
+}
+
+export default routes
